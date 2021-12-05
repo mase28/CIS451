6c98f2f2c4ac5e1f7e85068401d3376d280af865
Mason Olsen <olsenm2828@gmail.com>
2021-12-05 15:43:04 -0800

    f

--- database.sql
+++ database.sql
@@ -1,21 +1,21 @@
 CREATE DATABASE IF NOT EXISTS nba2020;
 USE nba2020;
 
-DROP TABLE IF EXISTS "nba2020"."players";
-CREATE TABLE "nba2020"."players" (
-	"p_id" bigint(20) unsigned NOT NULL auto_increment,
-	"fname" char(15) default NULL,
-	"lname" char(15) default NULL,
-	"total_points" mediumint(10) default NULL,
-	"avg_ppg" smallint(6) default NULL,
-	"total_rebounds" mediumint(10) default NULL,
-	"avg_reb" smallint(6) default NULL,
-	"total_assists" mediumint(10) default NULL,
-	"avg_assists" smallint(6) default NULL,
-	"total_steals" mediumint(10) default NULL,
-	"avg_steals" smallint(6) default NULL,
-	"total_long" mediumint(10) default NULL,
-	"avg_long" smallint(6) default NULL,
-	PRIMARY KEY ("p_id"),
-	UNIQUE KEY ("p_id")
+DROP TABLE IF EXISTS 'nba2020'.'players';
+CREATE TABLE 'nba2020'.'players' (
+	'p_id' bigint(20) unsigned NOT NULL auto_increment,
+	'fname' char(15) default NULL,
+	'lname' char(15) default NULL,
+	'total_points' mediumint(10) default NULL,
+	'avg_ppg' smallint(6) default NULL,
+	'total_rebounds' mediumint(10) default NULL,
+	'avg_reb' smallint(6) default NULL,
+	'total_assists' mediumint(10) default NULL,
+	'avg_assists' smallint(6) default NULL,
+	'total_steals' mediumint(10) default NULL,
+	'avg_steals' smallint(6) default NULL,
+	'total_long' mediumint(10) default NULL,
+	'avg_long' smallint(6) default NULL,
+	PRIMARY KEY ('p_id'),
+	UNIQUE KEY 'p_id' ('p_id')
 	) ENGINE=InnoDB DEFAULT CHARSET=latin1;
